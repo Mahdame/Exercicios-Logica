dc0f02670f2560cd0e562394a2fd1fc281dcae21
mahdame <mairamourabarbosa@gmail.com>
2020-06-06 14:08:01 -0300

    feat: ExerciciosLogica funcionando

--- ExerciciosLogica/Program.cs
+++ ExerciciosLogica/Program.cs
@@ -2,11 +2,51 @@
 
 namespace ExerciciosLogica
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public int cod, qtd, valor;
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            new Program().Input();
+        }
+
+        public void Input()
+        {
+            Console.WriteLine("Cod. do produto: ");
+            string codStr = Console.ReadLine();
+
+            Console.WriteLine("Quantidade do produto: ");
+            string qtdStr = Console.ReadLine();
+
+            Console.WriteLine("Valor unitário do produto: ");
+            string valorStr = Console.ReadLine();
+
+            if (!int.TryParse(codStr, out cod))
+            {
+                Console.WriteLine("Caracter nao aceito!");
+                return;
+            }
+
+            if (!int.TryParse(qtdStr, out qtd))
+            {
+                Console.WriteLine("Caracter nao aceito!");
+                return;
+            }
+
+            if (!int.TryParse(valorStr, out valor))
+            {
+                Console.WriteLine("Caracter nao aceito!");
+                return;
+            }
+
+            CalculoProduto();
+        }
+
+        public void CalculoProduto()
+        {
+            int total = (qtd * valor);
+            Console.WriteLine($"Total: R${total}");
         }
     }
 }
